65053136921fac561613d72236131f57f3df8ec0
Hannes <83277529+TheXample@users.noreply.github.com>
2023-10-05 13:31:52 +0200

    Update State.java

--- State.java
+++ State.java
@@ -1,7 +1,7 @@
 public enum State {
     Setup(Hangman.HANGMAN_8), Stage_One(Hangman.HANGMAN_7), Stage_Two(Hangman.HANGMAN_6),
     Stage_Three(Hangman.HANGMAN_5), Stage_Four(Hangman.HANGMAN_4),
-    Stage_Seven(Hangman.HANGMAN_3), Stage_Eight(Hangman.HANGMAN_2), Stage_Nine(Hangman.HANGMAN_1),
+    Stage_Five(Hangman.HANGMAN_3), Stage_Six(Hangman.HANGMAN_2), Stage_Seven(Hangman.HANGMAN_1),
     Game_Over(Hangman.HANGMAN);
 
     public final String[] hangman;
